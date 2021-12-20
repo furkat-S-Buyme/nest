f49100a37cbcc0553ba695b7bea695f9fecdf81f
furkat-sur-purple <furkat.sur@prpl.global>
2021-12-20 10:09:09 +0500

    hh integration module, schedule

--- src/top-page/top-page.service.ts
+++ src/top-page/top-page.service.ts
@@ -3,6 +3,7 @@ import { ModelType } from '@typegoose/typegoose/lib/types';
 import { InjectModel } from 'nestjs-typegoose';
 import { CreateTopPageDto } from './dto/create-top-page.dto';
 import { TopLevelCategory, TopPageModel } from './top-page.model';
+import { addDays } from 'date-fns';
 
 @Injectable()
 export class TopPageService {
@@ -35,6 +36,16 @@ export class TopPageService {
 
 	async updateById(id: string, dto: CreateTopPageDto) {
 		return this.topPageModel.findByIdAndUpdate(id, dto, { new: true }).exec();
+	}
+
+	async findForHhUpdate(date: Date) {
+		return this.topPageModel.find({
+			firstCategory: 0,
+			$or: [
+				{ 'hh.updatedAt': { $lt: addDays(date, -1) } },
+				{ 'hh.updatedAt': { $exists: false } },
+			],
+		}).exec();
 	}
 
 }
